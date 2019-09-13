c5c6cbfe67aa9dbe0e63e1bfa8d0d1ccbe43b02e
miroslav-sc <miroslavs@safecharge.com>
2019-09-13 14:34:16 +0300

    files fixed by WC standarts

--- SC_Versions_Resolver.php
+++ SC_Versions_Resolver.php
@@ -1,182 +1,174 @@
 <?php
-/* 
+/*
  * A try to get what we need when work with different versions of WooCommers.
  * For the moment the plugin lowest support version is 3.0.0
- * 
+ *
  * SafeCharge
  * 2018-08
  */
-class SC_Versions_Resolver
-{
-    /**
-     * Function process_admin_options
-     * Add hook to save admin options
-     * 
-     * @param WC_SC $_this - object of type WC_SC
-     */
-    public static function process_admin_options($_this)
-    {
-        add_action(
-            'woocommerce_update_options_payment_gateways_' . $_this->id,
-            array( &$_this, 'process_admin_options' )
-        );
-    }
-    
-    /**
-     * Function get_order_data
-     * Extract the data from the order
-     * 
-     * @param WC_Order $order
-     * @param string $key - a key name to extract
-     */
-    public static function get_order_data($order, $key = 'completed_date')
-    {
-        switch($key) {
-            case 'completed_date':
-                return $order->get_date_completed() ?
-                    gmdate( 'Y-m-d H:i:s', $order->get_date_completed()->getOffsetTimestamp() ) : '';
+class SC_Versions_Resolver {
 
-            case 'paid_date':
-                return $order->get_date_paid() ?
-                    gmdate( 'Y-m-d H:i:s', $order->get_date_paid()->getOffsetTimestamp() ) : '';
+	/**
+	 * Function process_admin_options
+	 * Add hook to save admin options
+	 *
+	 * @param WC_SC $_this - object of type WC_SC
+	 */
+	public static function process_admin_options( $_this) {
+		add_action(
+			'woocommerce_update_options_payment_gateways_' . $_this->id,
+			array( &$_this, 'process_admin_options' )
+		);
+	}
+	
+	/**
+	 * Function get_order_data
+	 * Extract the data from the order
+	 *
+	 * @param WC_Order $order
+	 * @param string $key - a key name to extract
+	 */
+	public static function get_order_data( $order, $key = 'completed_date') {
+		switch ($key) {
+			case 'completed_date':
+				return $order->get_date_completed() ?
+					gmdate('Y-m-d H:i:s', $order->get_date_completed()->getOffsetTimestamp()) : '';
 
-            case 'modified_date':
-                return $order->get_date_modified() ?
-                    gmdate( 'Y-m-d H:i:s', $order->get_date_modified()->getOffsetTimestamp() ) : '';
+			case 'paid_date':
+				return $order->get_date_paid() ?
+					gmdate('Y-m-d H:i:s', $order->get_date_paid()->getOffsetTimestamp()) : '';
 
-            case 'order_date';
-                return $order->get_date_created() ?
-                    gmdate( 'Y-m-d H:i:s', $order->get_date_created()->getOffsetTimestamp() ) : '';
+			case 'modified_date':
+				return $order->get_date_modified() ?
+					gmdate('Y-m-d H:i:s', $order->get_date_modified()->getOffsetTimestamp()) : '';
 
-            case 'id':
-                return $order->get_id();
+			case 'order_date':
+				return $order->get_date_created() ?
+					gmdate('Y-m-d H:i:s', $order->get_date_created()->getOffsetTimestamp()) : '';
 
-            case 'post':
-                return get_post( $order->get_id() );
+			case 'id':
+				return $order->get_id();
 
-            case 'status':
-                return $order->get_status();
+			case 'post':
+				return get_post($order->get_id());
 
-            case 'post_status':
-                return get_post_status( $order->get_id() );
+			case 'status':
+				return $order->get_status();
 
-            case 'customer_message':
-            case 'customer_note':
-                return $order->get_customer_note();
+			case 'post_status':
+				return get_post_status($order->get_id());
 
-            case 'user_id':
-            case 'customer_user':
-                return $order->get_customer_id();
+			case 'customer_message':
+			case 'customer_note':
+				return $order->get_customer_note();
 
-            case 'tax_display_cart':
-                return get_option( 'woocommerce_tax_display_cart' );
+			case 'user_id':
+			case 'customer_user':
+				return $order->get_customer_id();
 
-            case 'display_totals_ex_tax':
-                return 'excl' === get_option( 'woocommerce_tax_display_cart' );
+			case 'tax_display_cart':
+				return get_option('woocommerce_tax_display_cart');
 
-            case 'display_cart_ex_tax':
-                return 'excl' === get_option( 'woocommerce_tax_display_cart' );
+			case 'display_totals_ex_tax':
+				return 'excl' === get_option('woocommerce_tax_display_cart');
 
-            case 'cart_discount':
-                return $order->get_total_discount();
+			case 'display_cart_ex_tax':
+				return 'excl' === get_option('woocommerce_tax_display_cart');
 
-            case 'cart_discount_tax':
-                return $order->get_discount_tax();
+			case 'cart_discount':
+				return $order->get_total_discount();
 
-            case 'order_tax':
-                return $order->get_cart_tax();
+			case 'cart_discount_tax':
+				return $order->get_discount_tax();
 
-            case 'order_shipping_tax':
-                return $order->get_shipping_tax();
+			case 'order_tax':
+				return $order->get_cart_tax();
 
-            case 'order_shipping':
-                return $order->get_shipping_total();
+			case 'order_shipping_tax':
+				return $order->get_shipping_tax();
 
-            case 'order_total':
-                return $order->get_total();
+			case 'order_shipping':
+				return $order->get_shipping_total();
 
-            case 'order_type':
-                return $order->get_type();
+			case 'order_total':
+				return $order->get_total();
 
-            case 'order_currency':
-                return $order->get_currency();
+			case 'order_type':
+				return $order->get_type();
 
-            case 'order_currency':
-                return $order->get_currency();
+			case 'order_currency':
+				return $order->get_currency();
 
-            case 'order_version':
-                return $order->get_version();
+			case 'order_currency':
+				return $order->get_currency();
 
-            case 'order_version':
-                return $order->get_version();
+			case 'order_version':
+				return $order->get_version();
 
-            default:
-                return get_post_meta( $order->get_id(), '_' . $key, true );
-        }
+			case 'order_version':
+				return $order->get_version();
 
-        // try to call {get_$key} method
-        if ( is_callable( array( $order, "get_{$key}" ) ) ) {
-            return $order->{"get_{$key}"}();
-        }
-    }
-    
-    /**
-     * Function get_page_id
-     * Get permalink, by page name
-     * 
-     * @param string $page
-     * @return string|false
-     * 
-     * @deprecated
-     */
-    public static function get_page_id($page)
-    {
-        return wc_get_page_permalink($page);
-    }
-    
-    /**
-     * Function get_redirect_url
-     * Get the redirect url for an order
-     * 
-     * @param WC_Order $order
-     * @return string
-     * 
-     * @deprecated
-     */
-    public static function get_redirect_url($order)
-    {
-        return add_query_arg(
-            array(
-                'order-pay' => self::get_order_data($order, 'id'),
-                'key' => self::get_order_data($order, 'order_key')
-            ),
-            self::get_page_id('pay')
-        );
-    }
-    
-    /**
-     * Function get_client_country
-     * 
-     * @param WC_Customer $client
-     * @return string
-     */
-    public static function get_client_country($client)
-    {
-        if ( version_compare( WOOCOMMERCE_VERSION, '3.0.0', '>' ) ) {
-            return $client->get_billing_country();
-        }
-        else {
-            return $client->get_country();
-        }
-    }
-    
-    public static function get_shipping($order)
-    {
-        if ( version_compare( WOOCOMMERCE_VERSION, '3.0.0', '>' ) ) {
-            return $order->get_shipping_total();
-        }
-        else {
-            return $order->get_total_shipping();
-        }
-    }
+			default:
+				return get_post_meta($order->get_id(), '_' . $key, true);
+		}
+
+		// try to call {get_$key} method
+		if (is_callable(array( $order, "get_{$key}" ))) {
+			return $order->{"get_{$key}"}();
+		}
+	}
+	
+	/**
+	 * Function get_page_id
+	 * Get permalink, by page name
+	 *
+	 * @param string $page
+	 * @return string|false
+	 *
+	 * @deprecated
+	 */
+	public static function get_page_id( $page) {
+		return wc_get_page_permalink($page);
+	}
+	
+	/**
+	 * Function get_redirect_url
+	 * Get the redirect url for an order
+	 *
+	 * @param WC_Order $order
+	 * @return string
+	 *
+	 * @deprecated
+	 */
+	public static function get_redirect_url( $order) {
+		return add_query_arg(
+			array(
+				'order-pay' => self::get_order_data($order, 'id'),
+				'key' => self::get_order_data($order, 'order_key')
+			),
+			self::get_page_id('pay')
+		);
+	}
+	
+	/**
+	 * Function get_client_country
+	 *
+	 * @param WC_Customer $client
+	 * @return string
+	 */
+	public static function get_client_country( $client) {
+		if (version_compare(WOOCOMMERCE_VERSION, '3.0.0', '>')) {
+			return $client->get_billing_country();
+		} else {
+			return $client->get_country();
+		}
+	}
+	
+	public static function get_shipping( $order) {
+		if (version_compare(WOOCOMMERCE_VERSION, '3.0.0', '>')) {
+			return $order->get_shipping_total();
+		} else {
+			return $order->get_total_shipping();
+		}
+	}
 }
--- sc_config.php
+++ sc_config.php
@@ -1,10 +1,10 @@
 <?php
 
-/* 
+/*
  * Put all Constants here.
- * 
+ *
  * 2018
- * 
+ *
  * SafeCharge
  */
 
@@ -19,110 +19,98 @@ $sc_test_endpoint_host = 'https://ppp-test.safecharge.com/ppp/api/v1';
 
 // URLs for session token
 define('SC_LIVE_SESSION_TOKEN_URL', 'https://secure.safecharge.com/ppp/api/v1/getSessionToken.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_SESSION_TOKEN_URL', $sc_test_endpoint_host . '/getSessionToken.do');
-}
-else {
-    define('SC_TEST_SESSION_TOKEN_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getSessionToken.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_SESSION_TOKEN_URL', $sc_test_endpoint_host . '/getSessionToken.do');
+} else {
+	define('SC_TEST_SESSION_TOKEN_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getSessionToken.do');
 }
 
 // get merchant payment methods URLs for REST API
 define('SC_LIVE_REST_PAYMENT_METHODS_URL', 'https://secure.safecharge.com/ppp/api/v1/getMerchantPaymentMethods.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_REST_PAYMENT_METHODS_URL', $sc_test_endpoint_host . '/getMerchantPaymentMethods.do');
-}
-else {
-    define('SC_TEST_REST_PAYMENT_METHODS_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getMerchantPaymentMethods.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_REST_PAYMENT_METHODS_URL', $sc_test_endpoint_host . '/getMerchantPaymentMethods.do');
+} else {
+	define('SC_TEST_REST_PAYMENT_METHODS_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getMerchantPaymentMethods.do');
 }
 
 // get user UPOs
 define('SC_LIVE_USER_UPOS_URL', 'https://secure.safecharge.com/ppp/api/v1/getUserUPOs.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_USER_UPOS_URL', $sc_test_endpoint_host . '/getUserUPOs.do');
-}
-else {
-    define('SC_TEST_USER_UPOS_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getUserUPOs.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_USER_UPOS_URL', $sc_test_endpoint_host . '/getUserUPOs.do');
+} else {
+	define('SC_TEST_USER_UPOS_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/getUserUPOs.do');
 }
 
 // refund REST URLs
 define('SC_LIVE_REFUND_URL', 'https://secure.safecharge.com/ppp/api/v1/refundTransaction.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_REFUND_URL', $sc_test_endpoint_host . '/refundTransaction.do');
-}
-else {
-    define('SC_TEST_REFUND_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/refundTransaction.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_REFUND_URL', $sc_test_endpoint_host . '/refundTransaction.do');
+} else {
+	define('SC_TEST_REFUND_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/refundTransaction.do');
 }
 
 // void URLs
 define('SC_LIVE_VOID_URL', 'https://secure.safecharge.com/ppp/api/v1/voidTransaction.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_VOID_URL', $sc_test_endpoint_host . '/voidTransaction.do');
-}
-else {
-    define('SC_TEST_VOID_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/voidTransaction.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_VOID_URL', $sc_test_endpoint_host . '/voidTransaction.do');
+} else {
+	define('SC_TEST_VOID_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/voidTransaction.do');
 }
 
 // payment URLs
 define('SC_LIVE_PAYMENT_URL', 'https://secure.safecharge.com/ppp/api/v1/paymentAPM.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_PAYMENT_URL', $sc_test_endpoint_host . '/paymentAPM.do');
-}
-else {
-    define('SC_TEST_PAYMENT_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/paymentAPM.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_PAYMENT_URL', $sc_test_endpoint_host . '/paymentAPM.do');
+} else {
+	define('SC_TEST_PAYMENT_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/paymentAPM.do');
 }
 
 // Cashier payments URLs
 define('SC_LIVE_CASHIER_URL', 'https://secure.safecharge.com/ppp/purchase.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_CASHIER_URL', 'https://ppp-test.safecharge.com/ppp/purchase.do');
-}
-else {
-    define('SC_TEST_CASHIER_URL', 'https://ppp-test.safecharge.com/ppp/purchase.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_CASHIER_URL', 'https://ppp-test.safecharge.com/ppp/purchase.do');
+} else {
+	define('SC_TEST_CASHIER_URL', 'https://ppp-test.safecharge.com/ppp/purchase.do');
 }
 
 // dynamic 3D payment
 define('SC_LIVE_D3D_URL', 'https://secure.safecharge.com/ppp/api/v1/dynamic3D.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_D3D_URL', $sc_test_endpoint_host . '/dynamic3D.do');
-}
-else {
-    define('SC_TEST_D3D_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/dynamic3D.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_D3D_URL', $sc_test_endpoint_host . '/dynamic3D.do');
+} else {
+	define('SC_TEST_D3D_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/dynamic3D.do');
 }
 
 // payment 3D
 define('SC_LIVE_P3D_URL', 'https://secure.safecharge.com/ppp/api/v1/payment3D.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_P3D_URL', $sc_test_endpoint_host . '/payment3D.do');
-}
-else {
-    define('SC_TEST_P3D_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/payment3D.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_P3D_URL', $sc_test_endpoint_host . '/payment3D.do');
+} else {
+	define('SC_TEST_P3D_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/payment3D.do');
 }
 
 // payment with CC (cards) - not used at the moment
 define('SC_LIVE_PAYMENT_CC_URL', 'https://secure.safecharge.com/ppp/api/v1/paymentCC.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_PAYMENT_CC_URL', $sc_test_endpoint_host . '/paymentCC.do');
-}
-else {
-    define('SC_TEST_PAYMENT_CC_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/paymentCC.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_PAYMENT_CC_URL', $sc_test_endpoint_host . '/paymentCC.do');
+} else {
+	define('SC_TEST_PAYMENT_CC_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/paymentCC.do');
 }
 
 // Settle the Order
 define('SC_LIVE_SETTLE_URL', 'https://secure.safecharge.com/ppp/api/v1/settleTransaction.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_SETTLE_URL', $sc_test_endpoint_host . '/settleTransaction.do');
-}
-else {
-    define('SC_TEST_SETTLE_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/settleTransaction.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_SETTLE_URL', $sc_test_endpoint_host . '/settleTransaction.do');
+} else {
+	define('SC_TEST_SETTLE_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/settleTransaction.do');
 }
 
 // Open Order URL
 define('SC_LIVE_OPEN_ORDER_URL', 'https://secure.safecharge.com/ppp/api/v1/openOrder.do');
-if(strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
-    define('SC_TEST_OPEN_ORDER_URL', $sc_test_endpoint_host . '/openOrder.do');
-}
-else {
-    define('SC_TEST_OPEN_ORDER_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/openOrder.do');
+if (strpos(@$_SERVER['HTTP_HOST'], 'gw-4u.com') !== false) {
+	define('SC_TEST_OPEN_ORDER_URL', $sc_test_endpoint_host . '/openOrder.do');
+} else {
+	define('SC_TEST_OPEN_ORDER_URL', 'https://ppp-test.safecharge.com/ppp/api/v1/openOrder.do');
 }
 
 // user CPanel URLs
@@ -149,4 +137,4 @@ define('SC_AUTH_CODE_KEY', '_authCode');
 define('SC_GW_TRANS_ID_KEY', '_relatedTransactionId');
 define('SC_GW_P3D_RESP_TR_TYPE', '_p3dRespTransactionType');
 
-define('SC_LOGS_DIR', dirname( __FILE__ ). DIRECTORY_SEPARATOR . 'logs'. DIRECTORY_SEPARATOR);
+define('SC_LOGS_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR);
